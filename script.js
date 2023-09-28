1d26b1624bf3c7696ebcbc8be948eadd8b576950
Muhammad Abdullah <abdulx125@gmail.com>
2023-09-28 11:35:45 -0700

    another one but no prompt

--- script.js
+++ script.js
@@ -16,9 +16,29 @@ function randomPassword(length) {
     return password;
 }
 
-var passwordLength = prompt("Enter the password length:");
-console.log(randomPassword(passwordLength));
+// var passwordLength = prompt("Enter the password length:");
+// console.log(randomPassword(passwordLength));
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // another way to generate random password
 
 
+function generatePassword() {
+    var passwordLength = 12;
+    var password = "";
+    var charSet = [
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        'abcdefghijklmnopqrstuvwxyz',
+        '0123456789',
+        '!@#$%^&*()-_+='
+    ];
+    for (var i = 0; i < passwordLength; i++) {
+        var randomSet = charSet[Math.floor(Math.random() * charSet.length)];
+        password += randomSet.charAt(Math.floor(Math.random() * randomSet.length));
+    }
+    return password;
+}
+
+document.write("the random password is <br> " + generatePassword());
